6366b9744a86104a62fa8af2850dff17761f569f
Logan Bunch <loganbunch@gmail.com>
2023-05-27 18:34:13 -0700

    fully functional and responsive site built

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const html = document.querySelector("html");
 const themeMenu = document.getElementById("change-theme");
+const flexWrap = document.querySelector(".flex-wrapper");
+
+const intro = document.getElementById("intro-link")
+const about = document.getElementById("about-link")
+const exp = document.getElementById("exp-link")
+const projects = document.getElementById("projects-link")
+
+const copy = document.getElementById("copy")
+
+copy.innerHTML = "&copy;Copyright Logan Bunch " + new Date().getFullYear();
 
 window.addEventListener("load", function() {
   if (localStorage.theme) {
@@ -8,6 +18,7 @@ window.addEventListener("load", function() {
 })
 
 function changeTheme(theme) {
+  console.log("change theme to: " + theme)
   html.className = "";
   html.classList.add(theme)
   localStorage.setItem("theme", theme)
@@ -15,4 +26,53 @@ function changeTheme(theme) {
 
 function toggleThemeMenu() {
   themeMenu.classList.toggle("expand");
+  flexWrap.classList.toggle("show");
 }
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const intersecting = entry.isIntersecting
+    if (intersecting) {
+      console.log(entry)
+      switch (entry.target.id) {
+        case "intro":
+          intro.classList.add("active")
+          about.classList.remove("active")
+          exp.classList.remove("active")
+          projects.classList.remove("active")
+          break;
+        case "about":
+          intro.classList.remove("active")
+          about.classList.add("active")
+          exp.classList.remove("active")
+          projects.classList.remove("active")
+          break;
+        case "experience":
+          intro.classList.remove("active")
+          about.classList.remove("active")
+          exp.classList.add("active")
+          projects.classList.remove("active")
+          break;
+        case "projects":
+          intro.classList.remove("active")
+          about.classList.remove("active")
+          exp.classList.remove("active")
+          projects.classList.add("active")
+          break;
+        default:
+          intro.classList.remove("active")
+          about.classList.remove("active")
+          exp.classList.remove("active")
+          projects.classList.remove("active")
+
+      }
+    }
+  })
+}, {
+  threshold: 0.75,
+})
+
+
+document.querySelectorAll("section").forEach(section => {
+  observer.observe(section)
+})
